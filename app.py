6c67d06c5af9d90a357df015fb72cbf9fb21ac98
AnjaSlama <anja.slama@ucalgary.ca>
2021-10-20 02:19:34 -0600

    Create report form

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from forms import RegistrationForm, LoginForm, ReportForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY']='kh44ggg34lih3sd343hgfhfb7p04gdh4li'
+app.config['SECRET_KEY'] = 'cQeThWmZq4t7w!z%C*F-JaNdRgUjXn2r5u8x/A?D(G+KbPeShVmYp3s6v9y$B&E)'
 posts = [
     {
-        'author': 'Safety',
-        'title': 'News 1',
-        'content': 'First post content',
+        'category': 'Accident',
+        'title': 'Pedestrian dies after being stuck by a vehicle',
+        'content': 'Pedestrian dies after being stuck by a vehicle on dd/mm/yyyy. For more details, please visit: www....com.   ',
         'date_posted': 'October 15, 2021'
     },
     {
-        'author': 'Construction',
-        'title': 'News 2',
-        'content': 'Second post content',
+        'category': 'Update',
+        'title': 'Bridge open to pedestrian traffic',
+        'content': 'A new bridge open to pedestrian traffic on dd/mm/yyyy. For more details, please visit: www....com',
         'date_posted': 'October 16, 2021'
     }
 ]
@@ -28,7 +28,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html')
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -37,7 +37,7 @@ def register():
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -45,12 +45,23 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == 'admin@reporting.com' and form.password.data == '123':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('report'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+            flash(f'Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', form=form)
 
+@app.route("/report", methods=['GET', 'POST'])
+def report():
+    form = ReportForm()
+    if form.validate_on_submit():
+        flash(f'Report submitted for {form.category.data}!', 'success')
+        return redirect(url_for('report'))
+    return render_template('report.html', form=form)
+
+@app.route("/reports")
+def reports():
+    return render_template('reports.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- forms.py
+++ forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField, FileField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+REPORT_CATEGORIES =(
+    ("1", "Accident"),
+    ("2", "Traffic"),
+    ("3", "Hazard")
+)
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -20,3 +25,10 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class ReportForm(FlaskForm):
+    category= SelectField(label='Category', choices=REPORT_CATEGORIES)
+    title = StringField('Title', validators=[DataRequired(), Length(min=2, max=50)])
+    description = TextAreaField('Description', validators=[DataRequired() , Length(min=5, max=250)])
+    attachment = FileField('Attachment')
+    submit = SubmitField('Send')
