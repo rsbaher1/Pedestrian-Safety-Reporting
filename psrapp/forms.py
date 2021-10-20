582a60625d44dafe647d2d6d25741771c5fa6eb6
AnjaSlama <anja.slama@ucalgary.ca>
2021-10-20 10:37:06 -0600

    Update login/logout

--- psrapp/forms.py
+++ psrapp/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField, FileField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from psrapp.models import User
 
 REPORT_CATEGORIES =(
     ("1", "Accident"),
@@ -18,6 +19,18 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    # Control to avoid the registration with the same username or email address
+    def validate_username(self, username):
+        # Query the database
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
@@ -32,3 +45,4 @@ class ReportForm(FlaskForm):
     description = TextAreaField('Description', validators=[DataRequired() , Length(min=5, max=250)])
     attachment = FileField('Attachment')
     submit = SubmitField('Send')
+
